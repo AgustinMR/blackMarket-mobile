f07785f4edcf0b25af74776ca87c9306d975a83d
Agustin <agustinmr1995@gmail.com>
2017-12-09 11:24:28 -0300

    vista de inicio

--- src/pages/inicio.vue
+++ src/pages/inicio.vue
@@ -1,6 +1,17 @@
 <template>
     <f7-page name="inicio">
-        sdkfjb
+        <f7-navbar class="theme-black" sliding>
+            <f7-nav-left>
+                <f7-link icon="icon-bars" open-panel="left"></f7-link>
+            </f7-nav-left>
+            <f7-nav-center><img src="../assets/blackMarket.gif" style="height: 50px; margin-top: 25px"></f7-nav-center>
+        </f7-navbar>
+        <f7-panel left cover layout="dark" style="margin-top: 20px; padding: 0">
+            <f7-list>
+                <f7-list-button>Productos</f7-list-button>
+                <f7-list-button>Pedidos</f7-list-button>
+            </f7-list>
+        </f7-panel>
     </f7-page>
 </template>
 <script>
